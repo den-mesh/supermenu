83f8ee20e630a83342b2ba044fdce6aa79c9e6b5
Lada Penkova <whitethe2001@gmail.com>
2022-03-16 22:21:17 +0200

    Tabs

--- webpack.config.js
+++ webpack.config.js
@@ -47,10 +47,11 @@ module.exports = (env, argv) => {
                 filename: filename('css')
             }),
             new CopyWebpackPlugin({
-                patterns: [
-                  {from: path.resolve(__dirname, 'src/assets') , to: path.resolve(__dirname, 'dist/assets')}
-                ]
-              }),
+                patterns: [{
+                    from: path.resolve(__dirname, 'src/assets'),
+                    to: path.resolve(__dirname, 'dist/assets')
+                }]
+            }),
         ],
         module: {
             rules: [{
