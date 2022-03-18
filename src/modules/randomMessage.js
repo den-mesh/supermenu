90ded9bf156e2282ff4e50c57689a30d42ae034b
den-mesh <dmesh81@gmail.com>
2022-03-18 20:04:28 +0400

    Add randomMessage, fixed background.

--- src/modules/randomMessage.js
+++ src/modules/randomMessage.js
@@ -1,29 +1,61 @@
 import {Module} from '../core/modules';
-import { RandomArray } from '../utils';
+import {randomPath} from '../utils';
 
 export class RandomMessages extends Module {
-  constructor(type, text){
-    super(type, text)
-  }
+    constructor(type, text) {
+        super(type, text)
+    }
 
-  showMessage() {
-    const message = document.createElement('div');
-    message.style.position = 'absolute';
-    message.style.top = '20%';
-    message.style.padding  = '20px';
-    message.style.backgroundColor = 'green';
-    message.textContent = `Уважаемые разработчики данного курса и Куратор группы №20"Премиум" Андрей 
-    Наша команда Тахир , Денис, Лада и Алекс выражаем вам огромную благодарность за проделанный труд =)
-    Мы очень рады что учимся у вас и надеемся стать хорошими разработчиками!!!
-    Но к сожалению нам не хватило времени чтобы разработать оставшиеся таски. В дальнейшем мы доделаем проект!
-    Эот проект дал много понимания о командной разработке и хотелось бы практиковать такое на курсе =)
-     -------------Это окно закроется через 10 секунд----------------`;
+    showMessage() {
+        const MESSAGE = 'https://type.fit/api/quotes';
+        const dataContainer = document.querySelector('.random__msg');
+        dataContainer.style.visibility = 'visible';
+
+        const toggleLoader = () => {
+            const loaderHTML = document.querySelector('#loader');
+            const isHidden = loaderHTML.hasAttribute('hidden');
+
+            if (isHidden) {
+                loaderHTML.removeAttribute('hidden');
+            } else {
+                loaderHTML.setAttribute('hidden', '');
+            }
+        }
+
+        const errorMessage = () => {
+            const errorHTML = document.querySelector('#error');
+            const isHidden = errorHTML.hasAttribute('hidden');
+
+            if (isHidden) {
+                errorHTML.removeAttribute('hidden');
+            } else {
+                errorHTML.setAttribute('hidden', '');
+            }
+        }
+
+        const renderMessage = async () => {
+            try {
+                toggleLoader();
+                const response = await fetch(MESSAGE);
+                const messages = await response.json();
+
+                const randomMessage = randomPath(messages);
+                dataContainer.style.backgroundColor = '#9c27b0';
+                dataContainer.append(randomMessage.text);
 
 
-    document.body.append(message);
+            } catch (error) {
+                errorMessage();
+            } finally {
+                toggleLoader();
+            }
 
-    setTimeout( () => {
-      message.remove();
-    }, 10000)
-  }
+        }
+
+        renderMessage();
+
+        setTimeout(() => {
+            dataContainer.style.visibility = 'hidden';
+        }, 3000)
+    }
 }
